511b0f05d3778a3af9bf37a40795449ebb0b1087
Just-Prog <JustProg5835@outlook.com>
2026-06-04 10:20:14 +0800

    feat: 微调背景图切换展示

--- src/components/home/Background.tsx
+++ src/components/home/Background.tsx
@@ -5,22 +5,30 @@ const MainBackground = ({ current }: { current: number }) => {
   const BGs = [BG_AK, BG_ENDFIELD, BG_PMPM, ""];
   // useEffect(() => {}, []);
   return (
-    <main
-      style={{
-        objectFit: "contain",
-        backgroundImage: `url(${BGs[current]})`,
-        backgroundSize: "100svw 100svh",
-        paddingTop: "env(titlebar-area-height, 32px)",
-        width: "100svw",
-        height: "100svh",
-        overflow: "hidden",
-        zIndex: -10,
-        position: "absolute",
-        top: 0,
-        left: 0,
-        transition: "ease-in-out 150ms",
-      }}
-    />
+    <>
+      {BGs.map((v, k) => {
+        return (
+          <div
+            style={{
+              opacity: current === k ? 1 : 0,
+              objectFit: "contain",
+              backgroundImage: `url(${v})`,
+              backgroundSize: "100svw 100svh",
+              paddingTop: "env(titlebar-area-height, 32px)",
+              width: "100svw",
+              height: "100svh",
+              overflow: "hidden",
+              zIndex: -10,
+              position: "absolute",
+              top: 0,
+              left: 0,
+              transition: "ease-in-out 150ms",
+              transitionBehavior: "allow-discrete",
+            }}
+          />
+        );
+      })}
+    </>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,6 +12,11 @@ const HomeView = () => {
         <div className="absolute right-6 top-12 ">
           <GameSelector setCurrentGame={setCurrentGame} />
         </div>
+        <div className="absolute right-6 bottom-6">
+          <div className="w-64 h-20 flex flex-1 justify-center items-center bg-white/65 backdrop-blur-sm overflow-clip rounded-xl hover:bg-white/85 ease-in-out">
+            <span className="font-bold text-xl">启动游戏</span>
+          </div>
+        </div>
       </div>
     </div>
   );
