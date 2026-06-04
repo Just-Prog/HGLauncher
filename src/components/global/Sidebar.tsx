16eed32d167fe3e3e9b1d062b396459a9500a17c
Just-Prog <JustProg5835@outlook.com>
2026-06-04 15:05:58 +0800

    feat: sidebar高亮

--- src/components/global/Sidebar.tsx
+++ src/components/global/Sidebar.tsx
@@ -1,6 +1,13 @@
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Button from "@mui/material/Button";
-import { Outlet, useNavigate } from "react-router";
+import {
+  Outlet,
+  useHref,
+  useLocation,
+  useNavigate,
+  useResolvedPath,
+  useRoutes,
+} from "react-router";
 import { ReactNode, useEffect } from "react";
 import GamesIcon from "@mui/icons-material/Games";
 import InfoIcon from "@mui/icons-material/Info";
@@ -9,13 +16,21 @@ interface SideBarItemProps {
   icon: ReactNode;
   desc: string;
   href: string;
+  selected?: boolean;
 }
 
-const SideBarItem: React.FC<SideBarItemProps> = ({ icon, href, desc }) => {
+const SideBarItem: React.FC<SideBarItemProps> = ({
+  icon,
+  href,
+  desc,
+  selected,
+}) => {
   const nav = useNavigate();
   return (
     <Button variant="text" onClick={() => nav(href)}>
-      <div className="flex flex-col gap-y-1 text-xs text-black text-shadow-black/10">
+      <div
+        className={`flex flex-col gap-y-1 text-xs ${selected ? "text-blue-500/79 text-shadow-black/10" : "text-black text-shadow-black/10"}`}
+      >
         <span className="text-shadow-sm" style={{ fontSize: 24 }}>
           {icon}
         </span>
@@ -42,17 +57,19 @@ const SideBar = () => {
     },
   ];
 
+  const location = useLocation();
+
   return (
     <div className="w-20 h-[calc(100svh-32px)] min-h-full bg-white/78 backdrop-blur-[2px] flex items-center flex-col py-4 gap-y-2">
       <div className="flex-col flex items-center">
         {top_items.map((v) => (
-          <SideBarItem {...v} />
+          <SideBarItem {...v} selected={v.href === location.pathname} />
         ))}
       </div>
       <div className="flex-1" />
       <div className="flex-col flex items-center">
         {bottom_items.map((v) => (
-          <SideBarItem {...v} />
+          <SideBarItem {...v} selected={v.href === location.pathname} />
         ))}
       </div>
     </div>
@@ -63,7 +80,7 @@ const SideBarLayout = () => {
   return (
     <div className="flex flex-1 flex-row pt-8">
       <SideBar />
-      <div className="w-full flex-1">
+      <div className="w-full h-full flex-1">
         <Outlet />
       </div>
     </div>
